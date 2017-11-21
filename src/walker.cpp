4cca970f7d8bedbdefef9b635797381e55f126c8
Pranav Inani <pranavinani94@gmail.com>
2017-11-21 10:05:37 -0800

    Added source file the implements the walker class

---
+++ src/walker.cpp
@@ -0,0 +1,57 @@
+/*
+ * BSD 2-Clause License
+ *
+ * Copyright (c) 2017, Pranav Inani
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *  list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *  this list of conditions and the following disclaimer in the documentation
+ *  and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+/**
+ *  @file walker.cpp
+ *
+ *  @brief Implementation of walker class
+ *
+ *  @author Pranav Inani
+ *  @copyright 2017
+ */
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "sensor_msgs/LaserScan.h"
+#include "geometry_msgs/Twist.h"
+#include "walker.h"
+
+void Walker::callback(const sensor_msgs::LaserScan::ConstPtr& input) {
+  float min = 10;
+  for (const auto& dist : input->ranges) {
+    if (min > dist) {
+      min = dist;
+    }
+  }
+  obstDist = min;
+  ROS_INFO_STREAM("Distance to obstacle: " << obstDist);
+}
+
+float Walker::getObstDist() {
+  return obstDist;
+}
+
+
